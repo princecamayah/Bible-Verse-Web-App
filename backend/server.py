0244af0fe1e83f25baa581adedf23efbd7e5faad
Prince Camayah <princecamayah5@gmail.com>
2024-08-28 17:37:32 +0100

    imported config into server.py

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,4 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
-app.config["SQLACHEMCY_TRACK_MODIFICATIONS"] = False
-
-# database instance which acts as an ROM i.e. an object/class
-db = SQLAlchemy(app)
+from config import app, db
 
 if __name__ == "__main__":
     with app.app_context(): # if the database is not yet created then it must be created
