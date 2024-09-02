c383de83af1b18f93667da398097b2af216bf66b
Prince Camayah <princecamayah5@gmail.com>
2024-09-02 15:54:06 +0100

    created random_verse route to fetch a random verse from Verse database

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,20 @@
+from flask import jsonify
 from config import app, db
+from model import Verse, populate_database
+
+@app.route("/api/random_verse", methods=["GET"])
+def get_random_verse():
+    random_verse = Verse.query.order_by(db.func.random()).first()
+    if random_verse:
+        response = jsonify(random_verse.to_dict())
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        return jsonify({"error": "No verses available"}), 404
 
 if __name__ == "__main__":
     with app.app_context(): # if the database is not yet created then it must be created
         db.create_all()
+        populate_database()
         
     app.run(debug=True)
