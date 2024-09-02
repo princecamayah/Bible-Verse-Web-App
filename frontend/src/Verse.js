87b39e4cca17ad368076f14c7f3455d84ebe3df9
Prince Camayah <princecamayah5@gmail.com>
2024-09-02 16:49:44 +0100

    added a bible translation variable

--- frontend/src/Verse.js
+++ frontend/src/Verse.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 function Verse() {
+    // taken from bible-api using random_verse api data
     const [text, setText] = useState("");
     const [reference, setReference] = useState("");
+    const [loading, setLoading] = useState(true);
+    
+    // taken from random_verse api
     const [book, setBook] = useState("");
     const [chapter, setChapter] = useState("");
     const [verse, setVerse] = useState("");
-    const [loading, setLoading] = useState(true);
+
+    // by default NKJV but user can change it
+    const [translation, setTranslation] = useState("kjv")
+    
 
     // useEffect causes whatever is contained to run on every render e.g. if the state changes
     // however, in this case we use a [] to indicate that we only need to render once, since we are just fetching data
@@ -26,7 +33,7 @@ function Verse() {
 
         const fetchText = async () => {
             try {
-                const response = await fetch(`https://bible-api.com/${book}+${chapter}:${verse}`);
+                const response = await fetch(`https://bible-api.com/${book}+${chapter}:${verse}?translation=${translation}`);
                 const data = await response.json();
                 setText(data.text);
                 setReference(data.reference);
