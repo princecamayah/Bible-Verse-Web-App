4e117fe3f450273682ed07b047e5aa68a43d0363
Prince Camayah <princecamayah5@gmail.com>
2024-09-03 16:31:05 +0100

    created frontend Register component

---
+++ frontend/src/Register.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleRegister = async (e) => {
+        e.preventDefault(); // we don't want the page to reload so we stop it
+
+        try {
+            const response = await fetch('/register', {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ "email": email, "password": password }),
+            })
+
+            // if the response was successful, we return a positive status, otherwise we inform the user of the issue
+            const data = response.json();
+            if (response.ok) {
+                setMessage(data.message);
+            } else {
+                setMessage(data.error);
+            }
+        } catch (error) {
+            setMessage("Error: ", error);
+        }
+    }
+
+    return <div>
+        <h2>Register</h2>
+        <form onSubmit={handleRegister}>
+            {/* e is the event, e.target is the input field itself and e.target.value is the value within the input field */}
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required /> 
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </form>
+        <p>{message}</p>
+    </div>
+
+}
+
+export default Register;
