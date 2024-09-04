0056f31ccfc583bf8d81956862621882922e254d
Prince Camayah <princecamayah5@gmail.com>
2024-09-04 20:03:57 +0100

    added temporary hard-coded secret key

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_login import LoginManager
+import secrets
 
 app = Flask(__name__)
 CORS(app)
+ 
+app.config['SECRET_KEY'] = "temporary_key"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 app.config["SQLACHEMCY_TRACK_MODIFICATIONS"] = False
