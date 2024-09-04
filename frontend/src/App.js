d92cb9395b35881e0889db18c283d2d78cbde3bc
Prince Camayah <princecamayah5@gmail.com>
2024-09-04 13:34:08 +0100

    added Login and Register component to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Verse from "./Verse"
+import Register from "./Register"
+import Login from "./Login"
 
 function App() {
   return <div>
+    <Register />
+    <Login />
     <Verse />
   </div>;
 }
