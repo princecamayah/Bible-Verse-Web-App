8e3449dfa79877151d305e618579a8e01033ed01
Prince Camayah <princecamayah5@gmail.com>
2024-09-05 16:37:47 +0100

    added logout component to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import Verse from "./Verse"
-import Register from "./Register"
-import Login from "./Login"
+import Verse from "./Verse";
+import Register from "./Register";
+import Login from "./Login";
+import Logout from "./Logout";
 
 function App() {
-  return <div>
-    <Register />
-    <Login />
-    <Verse />
-  </div>;
+    return (
+        <div>
+            <Register />
+            <Login />
+            <Logout />
+            <Verse />
+        </div>
+    );
 }
 
 export default App;
