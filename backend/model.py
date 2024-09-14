db8ca1dca0f1a13cb0b381bf6e11238ea6d6b2ba
Prince Camayah <princecamayah5@gmail.com>
2024-09-14 15:42:54 +0100

    created a many-to-many realtionship between User model and Verse model in order to add a favourites list to the user

--- backend/model.py
+++ backend/model.py
@@ -2,12 +2,25 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 from config import db
 
+# many to many relationship between User and Verse (association table)
+# ForeignKey links each user_id to the one in the User table and likewise for verses
+# primary_key attribute set to true as we desire uniqueness for user_id and verse_id since we don't want the same verse to be favourited twice
+user_favourites = db.Table(
+    "user_favourites",
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("verse_id", db.Integer, db.ForeignKey("verse.id"), primary_key=True)
+)
+
 # User inherits from UserMixin which automatically includes methods like is_authenticated, get_id and more. 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(16), nullable=False, default="user") # role can be either user or admin
+
+    # the secondary attribute indicates to SQLAlchemy that user_favourites is the association table we want to query when accessing a user's favourites attribute
+    # lazy is by default "select" but "subquery" preloads the favourites attribute of the user to reduce query requests, making it more efficient
+    favourites = db.relationship('Verse', secondary=user_favourites, lazy="subquery")
 
 class Verse(db.Model):
     id = db.Column(db.Integer, primary_key=True)
