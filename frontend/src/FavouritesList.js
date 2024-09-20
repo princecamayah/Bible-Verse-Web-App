efd3531e036fc89360e2f40faaefd1362ee6630c
Prince Camayah <princecamayah5@gmail.com>
2024-09-20 23:39:48 +0100

    reconstructed FavouritesList component in order to allow for proper asynchronous rendering:  - use of forEach was not a consistent way to fetch all the texts, so substituted for Promise.all()  - changed fetchText() method to not populate the list but simply return the text and reference for each call, of which the fetchTextList method will create the textList

--- frontend/src/FavouritesList.js
+++ frontend/src/FavouritesList.js
@@ -5,32 +5,6 @@ function FavouritesList() {
     const [textList, setTextList] = useState([]);
     const [translation, setTranslation] = useState("kjv");
 
-    const fetchText = async (book, chapter, verse) => {
-        try {
-            const response = await fetch(
-                `https://bible-api.com/${book}+${chapter}:${verse}?translation=${translation}`
-            );
-            const data = await response.json();
-            if (response.ok) {
-                // react automatically knows that prevTextList is the old state of textList
-                setTextList((prevTextList) => [
-                    ...prevTextList,
-                    { text: data.text, reference: data.reference },
-                ]);
-            } else {
-                console.error("Response was not valid when fetching text.");
-            }
-        } catch (error) {
-            console.error(error.message);
-        }
-    };
-
-    const fetchTextList = (favouritesList) => {
-        favouritesList.forEach((value) => {
-            fetchText(value.book, value.chapter, value.verse);
-        });
-    };
-
     const fetchFavouritesList = async () => {
         try {
             const response = await fetch("/get_favourites");
@@ -47,12 +21,78 @@ function FavouritesList() {
         }
     };
 
+    const fetchText = async (book, chapter, verse) => {
+        try {
+            const response = await fetch(
+                `https://bible-api.com/${book}+${chapter}:${verse}?translation=${translation}`
+            );
+            const data = await response.json();
+            if (response.ok) {
+                return {
+                    text: data.text,
+                    reference: data.reference,
+                    details: { book: book, chapter: chapter, verse: verse },
+                };
+                // react automatically knows that prevTextList is the old state of textList
+                // setTextList((prevTextList) => [
+                //     ...prevTextList,
+                //     {
+                //         text: data.text,
+                //         reference: data.reference,
+                //         details: { book: book, chapter: chapter, verse: verse },
+                //     },
+                // ]);
+            } else {
+                console.error("Response was not valid when fetching text.");
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const fetchTextList = async (favouritesList) => {
+        // favouritesList.forEach(async (value) => {
+        //     await fetchText(value.book, value.chapter, value.verse);
+        // });
+        const fetchedTexts = await Promise.all(
+            favouritesList.map((value) =>
+                fetchText(value.book, value.chapter, value.verse)
+            )
+        );
+        setTextList(fetchedTexts);
+    };
+
+    // const removeFavourite = async (book, chapter, verse) => {
+    //     try {
+    //         const response = await fetch("/manage_favourite", {
+    //             method: "POST",
+    //             headers: { "Content-Type": "application/json" },
+    //             body: JSON.stringify({
+    //                 book: book,
+    //                 chapter: chapter,
+    //                 verse: verse,
+    //             }),
+    //         });
+    //         const data = await response.json();
+    //         if (response.ok) {
+    //             fetchFavouritesList();
+    //             console.log(data.message);
+    //         } else {
+    //             console.log(data.error);
+    //         }
+    //     } catch (error) {
+    //         console.error(error.message);
+    //     }
+    // };
+
     useEffect(() => {
         fetchFavouritesList();
     }, []);
 
     useEffect(() => {
-        fetchTextList(favouritesList);
+        if (favouritesList.length > 0) {
+            fetchTextList(favouritesList);
+        }
     }, [favouritesList]);
 
     return (
@@ -62,6 +102,17 @@ function FavouritesList() {
                 <div key={index}>
                     <p>{value.text}</p>
                     <small>{value.reference}</small>
+                    {/* <button
+                        onClick={() =>
+                            removeFavourite(
+                                value.details.book,
+                                value.details.chapter,
+                                value.details.verse
+                            )
+                        }
+                    >
+                        x
+                    </button> */}
                 </div>
             ))}
         </div>
